1918647065a9809dcb8cdd72d3ba0e67c831e923
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-02-15 13:44:59 -0600

    new flie

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,21 @@
+// class - defining the class
+// HelloWorld is the name of the class and it matches the name of the .java file
 public class HelloWorld {
+
+    //method- similar to a function in javascript
+    //main method --> specific method in java that acts as our entry point when running java
+    //(similar to starting point in a board game)
+    //like the front door of a house, you have to go through the door to get to any of the other rooms .
+
+    public static void main(String[] args){
+        //public - pinned for later
+        //static - pinned for later
+        //void - for now, it means this method doesn't expect anything to be refunded
+        //main - name of our method, main is keyword that is going to be searched as the entry point.
+        //String[] args - passing in a string array named args
+
+        //print out "hello world" to the console
+        System.out.println("cool");
+    }
+
 }
