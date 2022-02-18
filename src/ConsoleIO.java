bdd6c4df4720db13cfc5435430ac595d70c9b825
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-02-18 11:40:53 -0600

    finished exercises for String

--- src/ConsoleIO.java
+++ src/ConsoleIO.java
@@ -12,17 +12,24 @@ public class ConsoleIO {
 //        System.out.printf("%s, %s,", name, greeting);
 //
 
+//
+//       Scanner scanner = new Scanner(System.in); //create a Scanner object ,
+//       System.out.println("hello world, My name is ");
+//       String UserInput = scanner.nextLine();  //nextLine() will bring up user input( token)  with white space
+//       System.out.println ("oh Hello,"+UserInput+ ", nice to meet you");
+//
+//
+//       Scanner sc = new Scanner(System.in);
+//       System.out.println("Hello, My name is ");
+//       String UserName = scanner.next(); //next() will bring up only first token
+//       System.out.println ("oh Hello,"+UserName+ ", nice to meet you");
 
-       Scanner scanner = new Scanner(System.in); //create a Scanner object ,
-       System.out.println("hello world, My name is ");
-       String UserInput = scanner.nextLine();  //nextLine() will bring up user input( token)  with white space
-       System.out.println ("oh Hello,"+UserInput+ ", nice to meet you");
+         Scanner superman = new Scanner(System.in);
+       System.out.println("hello!@ ");
+       String reply = superman.nextLine();
+       System.out.println(reply);
 
 
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Hello, My name is ");
-       String UserName = scanner.next(); //next() will bring up only first token
-       System.out.println ("oh Hello,"+UserName+ ", nice to meet you");
 
 
 
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,10 +1,26 @@
+
+
 public class StringExercise {
     public static void main(String[] args){
 
         //1 String Basics
 
-        String sentence = "We don't need no education";
-        System.out.println(sentence.replace("education","though control"));
+        String ex1 = "We don't need no education\n" +
+                "We don't need no thought control" ;
+        System.out.println(ex1);
+
+        String ex2 ="Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(ex2);
+
+        String ex3 ="In windows, the main drive is usually C:\\";
+        System.out.println(ex3);
+
+        String ex4 ="I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(ex4);
+
+
+
 
 
     }
