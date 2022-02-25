3cc7f997e156a1ecce0cb2e15250a1388654789e
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-02-25 15:19:16 -0600

    oop exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -6,26 +6,47 @@ public class Input {
 
     private  Scanner scanner;
 
+    public Scanner getScanner(){
+        return scanner;
+    }
 
- public Input (){
-    this. scanner = new Scanner(System.in);
- }
 
- public String getString(){
-    return scanner.nextLine();
- }
- public int getInt(){
-     return scanner.nextInt();
- }
- public double getDouble(double min, double max){
-     double number  = scanner.nextDouble();
-     if(number > min && number < max){
-         return number;
-     }else{
-         getDouble(min, max);
+     public Input (){
+        this. scanner = new Scanner(System.in);
      }
-    return number;
- }
+
+     public String getString() {
+        return scanner.nextLine();
+     }
+
+     public int getInt(){
+         return scanner.nextInt();
+     }
+     public double getDouble(double min, double max){
+         System.out.println("enter floats");
+         double number  = scanner.nextDouble();
+         if(number > min && number < max){
+             return number;
+         }else{
+             getDouble(min, max);
+         }
+        return number;
+     }
+
+     public boolean yerNo(){
+         System.out.println("please enter [yes /no/y/n] ");
+         String answer = scanner.nextLine();
+         if(answer.equalsIgnoreCase("yes")|| answer.equalsIgnoreCase("y") ){
+             return true;
+         }else if(answer.equalsIgnoreCase("no") || answer.equalsIgnoreCase("n")){
+             return false;
+         }else{
+             return yerNo();
+         }
+     }
+
+
+
 
 public static void main(String[] args) {
 
