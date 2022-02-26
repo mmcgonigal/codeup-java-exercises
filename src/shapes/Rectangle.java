10a21cd48b75d50a4d759d612a3aa3890c903b49
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-02-26 17:25:57 -0600

    created Rentagle class

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,41 @@
+package shapes;
+
+public class Rectangle {
+    protected int length ;
+    protected int width;
+
+    public int getLength(){
+        return length;
+    }
+    public void setLength(int length){
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width){
+        this.width = width;
+        }
+
+
+    public Rectangle(int length, int width){
+    this.length = length;
+    this.width = width;
+    }
+
+    public int perimeter (int length,int width){
+        return (2*length)+(2*width);
+    }
+    public int area(int length, int width){
+        return length * width;
+    }
+
+
+
+
+
+
+
+}
+
