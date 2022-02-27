b12afefe446093bdab95a3e28450fd4394420ac9
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-02-26 18:11:54 -0600

    square refactore

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -3,17 +3,18 @@ package shapes;
 public class Square extends Rectangle{
     protected int side;
 
+
     public int getSide(){
         return side;
     }
     public void setSide(int side){
-        this.width = side;
-        this.length = side;
+        this.side = side;
+
     }
 
-    public Square(int length, int width, int side){
-        super(length, width);
-        this.length = side;
-        this.width = side;
+    public Square(int side){
+        super(side, side);
     }
+
+
 }
