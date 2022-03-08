e5604d362cd03ecb56387c71eb9dce9335945934
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-03-08 13:48:47 -0600

    update on Exception and  error handling

---
+++ src/Exception_lecture/ExceptionTest.java
@@ -0,0 +1,29 @@
+package Exception_lecture;
+
+public class ExceptionTest {
+    public static void main(String[] args) throws // this will find exeption
+            Exception{
+        System.out.println(" I am above the exception"); // this will run because this is  before Exception right below.
+      //  throw new Exception("Woops - Exception-error happened"); // this is the exception < throws  and throw difference>
+//        System.out.println("Good morning Sirius!"); // after Exception above , this won't run , so i cant see,this code anymore .
+
+        // Ok- that's an interesting experiment that shows an exception breaking a program and how it breaks,but how would i start to work around exception ?
+         // A - Look at a try - catch block !
+
+        try{
+            // Code to break application here/..
+            throw new RuntimeException("Something broke while running the application!!!!!!!!!!!!!!!");
+        } catch (NullPointerException exceptionObject){  // instead of RuntimeException , inside catch, it is NullpointerException//
+            System.out.println("here is an example of .getMessage() ---> " +exceptionObject.getMessage());
+            exceptionObject.printStackTrace();
+
+        } catch (RuntimeException exceptionObject){
+            System.out.println("this is a runtime exception");
+            System.out.println("Here is an example of .getMessage() -->" + exceptionObject.getMessage());
+            exceptionObject.printStackTrace();
+        } catch (Exception exceptionObject){
+            System.out.println("this is a generic, top loevel exception");
+            System.out.println("Here is an example if .getMessage() -> " + exceptionObject.getMessage());
+        }
+    }
+}
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,5 +1,6 @@
 package util;
 
+
 import java.util.Scanner;
 
 public class Input {
@@ -20,21 +21,36 @@ public class Input {
         return scanner.nextLine();
      }
 
-     public static int getInt(){
-         Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+     public static int getInt(){  // 이니셜라이징 getInt
+        // Scanner scanner = new Scanner(System.in);
+       // int numInt = scanner.nextInt();
+        // String numInt;
+        int num =0;
+         String numInt = getString(); // 이니셜라이징 getString method    그리고 그걸 numInt 에 넣음.
+        try { //   그리고 28 . 29 다음 이 라인을 퍼폼 해라 ,
+          num = Integer.parseInt(numInt);
+            return num;
+         }catch (Exception e){   // 만약 애러가 있을경우 , 그에러가 케치들 중에 하나 있을 경우 , 그 캐치 구문을 수행하면 됨 .
+            System.out.println("e =  invalid input" + e.getMessage() );
+            e.printStackTrace();
+        }
+        return num;
      }
 
      public static double getDouble(double min, double max){
-         Scanner scanner = new Scanner(System.in);
+         //Scanner scanner = new Scanner(System.in);
          System.out.println("enter floats");
-         double number  = scanner.nextDouble();
-         if(number > min && number < max){
-             return number;
+         double numDbl;
+         numDbl  = getString();
+         try{
+
+         }
+         if(numDbl > min && numDbl < max){
+             return numDbl;
          }else{
              getDouble(min, max);
          }
-        return number;
+        return numDbl;
      }
 
      public static boolean yesNo(){
